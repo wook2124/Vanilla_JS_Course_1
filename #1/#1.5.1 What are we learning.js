696dacca123a4c85b90c2376ac3ab52349218463
Beginner <wook2124@naver.com>
2020-02-14 11:58:48 +0000

    #1.5.1 What are we learning, Complete!

--- #1/#1.5.1 What are we learning.js
+++ #1/#1.5.1 What are we learning.js
@@ -1,4 +1,5 @@
 console.log("I'm Working. I'm JS. I'm worth it. I'm beautiful.");
 
 // python에서는 console.log가 print로 작동함
+// print 코드를 적은 상태로 script.js에 써져있으면 정말로 내가 적어둔 문서로 print가 실행됨..ㅋㅋㅋ(주석처리 끝까지 잘해주기 /* */)
 print("I'm Working. I'm JS. I'm worth it. I'm beautiful.")
--- script.js
+++ script.js
@@ -1,4 +1,1 @@
 console.log("I'm Working. I'm JS. I'm worth it. I'm beautiful.");
-
-// python에서는 console.log가 print로 작동함
-print("I'm Working. I'm JS. I'm worth it. I'm beautiful.")
