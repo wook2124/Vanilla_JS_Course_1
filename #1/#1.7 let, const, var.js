fcb59af5fa06afea5fa1cfa918ae862e0652711a
Beginner <wook2124@naver.com>
2020-02-16 09:22:30 +0000

    #1.8 Data Types on JS, Complete!

--- #1/#1.7 let, const, var.js
+++ #1/#1.7 let, const, var.js
@@ -1,5 +1,6 @@
 // let과 const의 차이점 - let은 변수값을 얼마든지 다른값으로 변경가능함, const는 변수값을 한번 정하면 다른값으로 변경이 불가능함
 // constant(상수)는 변수값을 변경할 수 없기 때문에 새로 a = 4로 변수값을 설정하면 에러가 난다. Assignment to constant variable.(상수변수에 할당하려고함.) 그리고 그것은 3번째 줄 3번째 칸의 오류임.
+// 첫 사용은 const로, 정말 필요할 때만 let을 사용하기.
 const a = 221;
 let b = a - 5;
 a = 4;
--- #1/#1.8 Data Types on JS.js
+++ #1/#1.8 Data Types on JS.js
@@ -1,25 +1,34 @@
-/* String
+// 한줄 주석처리(Comment)
+
+/* 
+여러
+문장
+주석
+처리
+*/
+
+// String 
+// '__' 안에 숫자를 넣으면 그것도 Text가 됨!
 const what = "126830, YoungWook";
 
 console.log(what);
-*/
 
-/* Boolean
+
+// Boolean
 // true = 1, false = 0 (이진법 binary)
 const what_ = true;
 const what__ = false;
 
 console.log(what_, what__);
-*/
 
-/* Number
+
+// Number
 const what = 666;
 
 console.log(what);
-*/
 
-/* Float
+
+// Float
 const what = 666.77;
 
 console.log(what);
-*/
