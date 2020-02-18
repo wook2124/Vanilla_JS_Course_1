55e8e30c97812887ff1aab8e5d6a78105d5fac7e
Beginner <wook2124@naver.com>
2020-02-18 13:36:08 +0000

    #2.2 JS DOM Functions, Complete!

---
+++ #2/#2.2 JS DOM Functions.js
@@ -0,0 +1,5 @@
+// javascript를 이용해서 title도 변경할 수 있다.
+// innerHTML은 title 안에있는 method임! DOM(Document Object Model)형태로 변경한 것!
+const title = document.getElementById("title");
+
+title.innerHTML = "Hi! From JS";
