6ea9d9a37d9162d6b8e7c3d5bc123fcb81ec9b8c
Beginner <wook2124@naver.com>
2020-02-18 13:35:20 +0000

    #2.2 JS DOM Functions, Complete!

--- script.js
+++ script.js
@@ -1,25 +1,3 @@
-const calculator = {
-  plus: function(a, b){
-    return a + b;
-  },
-  minus: function(a, b){
-    return a - b;
-  },
-  division: function(a, b){
-    return a / b;
-  },
-  times: function(a, b){
-    return a * b;
-  },
-  power: function(a, b){
-    return a ** b;
-  }
-}
+const title = document.getElementById("title");
 
-const plus = calculator.plus(10, 3);
-const minus = calculator.minus(10, 3);
-const division = calculator.division(10, 3);
-const times = calculator.times(10, 3);
-const power = calculator.power(10, 3);
-
-console.log(plus, minus, division, times, power);
+title.innerHTML = "Hi! From JS";
