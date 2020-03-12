a6e63374ea55b636615906c4c378df080a6bd639
wook2124 <wook2124@naver.com>
2020-03-12 19:50:43 +0900

    Update #1.10 Organizing Data with Objects.js

--- #1/#1.10 Organizing Data with Objects.js
+++ #1/#1.10 Organizing Data with Objects.js
@@ -1,5 +1,6 @@
-// {} - python의 dictionary 기능임!
-// 각각 Object(개체)로 묶어주는 것 - name, age, gender, isHandsome 각각 개체의 변수로써 존재함
+// {} - python의 dictionary 기능
+// 각각의 Object(개체)로 묶어주는 것
+// name, age, gender, isHandsome 각각, 개체의 변수로써 존재함
 const wookInfo = {
   name: "YoungWook",
   age: 27,
@@ -9,7 +10,9 @@ const wookInfo = {
 
 console.log(wookInfo);
 
-// wookInfo.gender - {}안의 각각의 변수를 호출하려면 '.변수' 만 뒤에 붙이면됨!
+
+// wookInfo.gender
+// {}안의 각각의 변수를 호출하려면 '.변수'
 const wookInfo = {
   name: "YoungWook",
   age: 27,
@@ -19,7 +22,8 @@ const wookInfo = {
 
 console.log(wookInfo.gender);
 
-// {} 안에 있는 변수는 const로 정해진 변수 안에 있는 것이라도 바꿀 수 있음!
+
+// {} 안에 있는 변수는 const로 정해진 변수선언 안에 있더라도 바꿀 수 있음
 const wookInfo = {
   name: "YoungWook",
   age: 27,
@@ -33,7 +37,9 @@ wookInfo.gender = "Female"
 
 console.log(wookInfo.gender);
 
-// wookInfo Object{} 안 favMovies Object는 Array[]로 정렬하고, favFood Object는 Array[]안에 다시 Object{}를 이용해서 Data를 정리함!
+
+// {} 안 favMovies Object는 Array[]로 정렬하고
+// favFood Object는 Array[]안에 다시 Object{}를 이용해서 Data를 정리함
 const wookInfo = {
   name: "YoungWook",
   age: 27,
@@ -53,7 +59,9 @@ const wookInfo = {
 
 console.log(wookInfo);
 
-// favFood Object{}는 Array[]로 되있기때문에 [0]을 이용해서 둘 중에 하나를 고를 수 있다. '.변수.변수'
+
+// favFood Object{}는 Array[] 이기때문에 
+// [0]을 이용해서 둘 중 하나를 택할 수 있음 - '.변수.변수'
 const wookInfo = {
   name: "YoungWook",
   age: 27,
@@ -72,6 +80,7 @@ const wookInfo = {
 };
 
 console.log(wookInfo.favFood[0].name);
+
 
 // 여러가지 한꺼번에 출력하기, 끝!
 const wookInfo = {
