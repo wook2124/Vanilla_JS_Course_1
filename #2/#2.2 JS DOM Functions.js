e106c6ca5c0d18cf88ea9f8f0684d06e4733a36a
wook2124 <wook2124@naver.com>
2020-03-15 19:01:15 +0900

    Update #2.2 JS DOM Functions.js

--- #2/#2.2 JS DOM Functions.js
+++ #2/#2.2 JS DOM Functions.js
@@ -1,4 +1,4 @@
-// JavasScript를 이용해서 HTML의 title도 변경가능함
+// JavaScript를 이용해서 HTML의 title도 변경가능함
 // innerHTML은 title 안에있는 method이고, 
 // DOM(Document Object Model)형태로 변경한 것임
 const title = document.getElementById("title");
