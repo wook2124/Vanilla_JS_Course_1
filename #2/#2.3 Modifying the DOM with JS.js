4045a0a40644b53fb98f1c625190c884f52f40e4
wook2124 <wook2124@naver.com>
2020-03-16 23:09:40 +0900

    Update #2.3 Modifying the DOM with JS.js

--- #2/#2.3 Modifying the DOM with JS.js
+++ #2/#2.3 Modifying the DOM with JS.js
@@ -1,10 +1,12 @@
-// console.dir(x) - dir을 입력해서 그 안에있는 object를 살펴볼 수 있음!
-// title.에서 .뒤에 title object 안에 있는 method를 입력할 수 있다.
-// document.querySelector 사용하기 - querySelector는 노드의 첫번째 자식을 반환한다. 만약 id로 찾고싶다면 #을 쓰고 class로 찾고싶다면 #을 쓴다. 
-// id는 object이고 class는 그 안에 속한 method를 뜻하는 것 같다... 아마??
+// document.querySelector
+// querySelector는 노드의 첫번째 자식을 반환함, 쉽게말해 document에서 찾는 것임
+// id를 찾을라면 #, class를 찾을라면 .을 명칭 앞에 붙임
 const title = document.querySelector("#title");
 
 title.innerHTML = "Hi! From JS";
 title.style.color = "red";
 document.title = "I own you now";
 console.dir(document);
+// console.dir(x)
+// method dir을 통해서 console 안에있는 모든 object를 살펴볼 수 있음
+// title.으로 title의 모든 object method를 입력할 수 있음
