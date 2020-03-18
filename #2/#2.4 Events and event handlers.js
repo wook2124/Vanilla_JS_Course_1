114830cfb4b0cbbaeb429c14583beef4cdf89db9
wook2124 <wook2124@naver.com>
2020-03-18 16:56:29 +0900

    Update #2.4 Events and event handlers.js

--- #2/#2.4 Events and event handlers.js
+++ #2/#2.4 Events and event handlers.js
@@ -1,6 +1,6 @@
-// window size가 변경될 때(resize 될 때) 만들어둔 handleResize 함수를 호출해줘!
-// 만약 handleResize()라고 적으면 함수를 즉시 호출하기 때문에, 다시말해 window resize가 변경이 안되도 이미 호출을 한 상태이기 때문에 ()를 쓰면 안된다!
-// ()는 버튼이다.
+// window.addEventListener
+// window에서 "resize" event가 발생하면 function handleResize를 호출함
+// handleResize()라고 적으면 function을 즉시 호출함
 const title = document.querySelector("#title");
 
 function handleResize(){
@@ -9,7 +9,8 @@ function handleResize(){
 
 window.addEventListener("resize", handleResize);
 
-// event가 발생할 때마다 어떤 event에 대해 반응했는지 출력이 되는 것을 알 수 있다.
+
+// event가 발생할 때마다, 어떤 event에 반응했는지 console에 출력됨
 const title = document.querySelector("#title");
 
 function handleResize(event){
@@ -18,7 +19,8 @@ function handleResize(event){
 
 window.addEventListener("resize", handleResize);
 
-// click event에 반응해서 title의 color가 green으로 되게끔 function을 설정함. 
+
+// "click" event가 발생하면 title.style.color가 green이 되게끔 function을 설정함 
 const title = document.querySelector("#title");
 
 function handleClick(){
