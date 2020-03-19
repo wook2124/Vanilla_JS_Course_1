91ca5211a023e4a9c200155eb1e444775d94d61c
wook2124 <wook2124@naver.com>
2020-03-20 00:10:32 +0900

    Update #2.5 If, else, and, or.js

--- #2/#2.5 If, else, and, or.js
+++ #2/#2.5 If, else, and, or.js
@@ -1,4 +1,5 @@
-// condition(조건)은 조건이 참을 의미하면 뭐든 넣을 수 있고, block은 console.log, alert 등 아무거나 와도 되는 것을 뜻한다.
+// condition(조건)은 조건이 ture(참)이면 뭐든 넣을 수 있고, 
+// block은 console.log, alert등 어떤게 와도 됨을 뜻함
 if(condition){
   block
 }
@@ -6,7 +7,8 @@ else {
   block
 }
 
-// 10이 5보다 큰 것은 true이기 때문에 Hi가 출력됨.
+
+// 10이 5보다 큰 것은 true(참)이기 때문에 Hi가 출력됨
 if(10 > 5){
   console.log("Hi");
 }
@@ -14,7 +16,9 @@ else {
   console.log("Ho");
 }
 
-// ===는 checking하는 것이다, ==하고는 다름! 그리고 이 경우에는 false이기 때문에 else가 출력됨.
+
+// ===는 checking하는 것임 (==하고는 다름) 
+// 이 경우에는 10 === 5가 false(거짓)이기 때문에 else Ho가 출력됨
 if(10 === 5){
   console.log("Hi");
 }
@@ -22,7 +26,9 @@ else {
   console.log("Ho");
 }
 
-// else if - if의 또다른 condition(조건), python에서는 elif로 쓰임. 추가로 '10'은 숫자가 아니라 str(문자열)임!
+
+// else if - if의 또다른 condition(조건) (Python에서는 elif로 쓰임) 
+// 추가로 "10"은 숫자가 아니라 str(문자열)임
 if("10" === 10){
   console.log("Hi");
 }
@@ -33,7 +39,8 @@ else {
   console.log("Ho");
 }
 
-// &&(And) - condition(조건)이 둘 다 true(참)이어야함.
+
+// &&(And) - condition(조건)이 둘 다 true(참)이어야함
 if("10" === "10" && "wook" === "zook"){
   console.log("Yes");
 }
@@ -41,7 +48,8 @@ else {
   console.log("No");
 }
 
-// ||(Or) - condition(조건) 두 개 중 적어도 하나는 true(참)이여야함.
+
+// ||(Or) - condition(조건)의 둘 중 적어도 하나는 true(참)이여야함
 if("10" === 10 || "wook" === "zook"){
   console.log("Yes");
 }
@@ -49,7 +57,8 @@ else {
   console.log("No");
 }
 
-// prompt - 뭔갈 물어보는 함수(이제는 거의 쓰지 않음. alert의 기능)
+
+// prompt - 뭔가를 물어보는 함수(이제는 거의 쓰지 않음, alert의 기능)
 const age = prompt("How old are you");
 
 if(age > 18) {
@@ -59,7 +68,8 @@ else {
   console.log("You can't!");
 }
 
-// 마지막 연습, 끝!
+
+// 마지막 연습
 const age = prompt("How old are you");
 
 if(age > 18 && age <= 22) {
