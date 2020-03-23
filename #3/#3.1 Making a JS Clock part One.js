44b036de364b189e3cf5245e4c87022b2f9c9e8e
wook2124 <wook2124@naver.com>
2020-03-23 09:38:31 +0900

    Update #3.1 Making a JS Clock part One.js

--- #3/#3.1 Making a JS Clock part One.js
+++ #3/#3.1 Making a JS Clock part One.js
@@ -1,4 +1,5 @@
-// clock 만들기! - div class='js-clock'과 들여쓰기로 h1 js-title을 만들어줌
+// clock 만들기
+// div class="js-clock"과 들여쓰기로 <h1> js-title을 만들어줌
 <!DOCTYPE html>
 <html>
   <head>
@@ -13,7 +14,9 @@
   </body>
 </html>
 
-// function init()으로 초기화(initialize)하고, clockContainer에 .js-clock class가 있는지, 그리고 h1도 같이 querySelector로 찾아줌.
+
+// function init()으로 초기화(initialize)하고
+// clockContainer로 .js-clock class와 h1을 각각 querySelector로 찾아줌
 const clockContainer = document.querySelector(".js-clock"), 
   clockTitle = clockContainer.querySelector("h1");
 
@@ -21,8 +24,11 @@ function init() {}
 
 init();
 
-// clockTitle.innerText - h1으로 js-title인 00시00분을 바꾸기 위한 것!(객체 안에 텍스트를 넣기 위함)
-// 백틱`__`으로 hours와 minutes를 argument화해서 시간으로 표시함! - $은 python에서 f(format)과 동일함.
+
+// clockTitle.innerText
+// h1으로 js-title인 00시00분을 바꾸기 위한 것(객체 안에 텍스트를 넣기 위함)
+// 백틱`__`으로 hours와 minutes를 argument화해서 시간으로 표시함
+// $은 Python에서 문장 앞에 써주던 f(format)과 동일함
 const clockContainer = document.querySelector(".js-clock"), 
   clockTitle = clockContainer.querySelector("h1");
 
@@ -37,7 +43,9 @@ function init() {}
 
 init();
 
-// seconds 까지 추가해서 결과값이 잘 나오긴 했지만 매번 새로고침을 해줘야함. 새로고침 안해도 작동할 수 있게 고쳐야됨! (매시간 업데이트를 할 수 있게하는 setInterval는 다음시간에!)
+
+// seconds까지 추가해서 결과값이 잘 나오긴 했지만 매번 새로고침을 해줘야함
+// 새로고침 안해도 매번 동적으로 업데이트를 할 수 있는 setInterval는 다음시간에
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
