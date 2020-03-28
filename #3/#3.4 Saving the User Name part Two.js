63f423a85062fb99a6de3a2f662a7e246b6e87ad
wook2124 <wook2124@naver.com>
2020-03-28 16:05:57 +0900

    Update #3.4 Saving the User Name part Two.js

--- #3/#3.4 Saving the User Name part Two.js
+++ #3/#3.4 Saving the User Name part Two.js
@@ -1,49 +1,17 @@
-// currentUser 값이 null인 경우에는 askForName을 통해서 form.classList에 추가(add)해주는 기능(function)을 만듦
+// form 빈칸에 text를 적고 Enter를 누르는 행동은 form을 submit(제출)하는 것
+// form을 제출하면 제출된 것을 다른 곳으로 보내려고 함
+// Enter를 누르면 text값이 default로 사라지고 다시 빈칸으로 됨
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greeting");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
-
-function askForName() {
-  form.classList.add(SHOWING_CN);
-}
-
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
-
-function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    askForName();
-  } else {
-    paintGreeting(currentUser);
-  }
-}
-
-function init() {
-  loadName();
-}
-
-init();
-
-// 빈칸에 text를 적고 Enter를 누르는 행동은 = form을 submit(제출)하는 것이다! - form을 제출하면 제출된 것을 다른 곳으로 보내려고 한다. (Enter를 누르면 text값이 default로 사라지고 다시 빈칸으로 됨)
-// handleSubmit(event)를 추가해서 submit(제출) event가 발생하면 행동할 규칙을 정해줌! - askForName에서 eventListener를 추가해서 submit이 발생하면 handleSummit 값을 호출하게끔 함.
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greeting");
-
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
-
+// handleSubmit(event)로 submit(제출) event가 발생하면 행동할 규칙을 정함
 function handleSubmit(event) {
   event.preventDefault();
 }
-
+// eventListener로 submit이 발생하면 handleSummit 값을 호출
 function askForName() {
   form.classList.add(SHOWING_CN);
   form.addEventListener("submit", handleSubmit);
@@ -57,6 +25,7 @@ function paintGreeting(text) {
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
+  // 만약(if) currentUser === null이면 askForName으로 <form> classList에 추가(add)
   if (currentUser === null) {
     askForName();
   } else {
@@ -70,99 +39,37 @@ function init() {
 
 init();
 
-// 다음으로 currentValue의 값을 알기 위해서 input.value를 이용해서 enter를 치면 console.log에 찍히게끔 함!
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greeting");
 
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
-
+// currentValue 값을 얻기 위해 input.value를 이용해 Enter를 쳐서 console.log로 출력해봄
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
   console.log(currentValue);
 }
 
-function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
-}
 
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
-
-function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    askForName();
-  } else {
-    paintGreeting(currentUser);
-  }
-}
-
-function init() {
-  loadName();
-}
-
-init();
-
-// paintGreeting(currentValue)로 현재 적은 값을 paintGreeting으로 보낸 다음 `Hello ${currentValue}`값이 나오게끔함!
-const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greeting");
-
-const USER_LS = "currentUser",
-  SHOWING_CN = "showing";
-
+// paintGreeting(currentValue)로 현재 적은 input.value를 
+// paintGreeting으로 보낸 다음 `Hello ${currentValue}`값이 나오게끔함
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
   paintGreeting(currentValue);
 }
 
-function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
-}
 
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
-
-function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-    askForName();
-  } else {
-    paintGreeting(currentUser);
-  }
-}
-
-function init() {
-  loadName();
-}
-
-init();
-
-// saveName(text) - submit(제출) event가 발생하면 Default를 prevent(방지)하고, paintGreeting과 saveName이 실행되어 currentValue(=input.value)로 LS에 저장됨!
-// localStorage.setItem(USER_LS, text)에서 USER_LS는 Key, text는 value이기 때문에 USER_LS로 지정한 currentUser가 Key로 가고, text로 지정한 YoungWook이 value로 가게됨
+// 최종 코드
 const form = document.querySelector(".js-form"),
   input = form.querySelector("input"),
   greeting = document.querySelector(".js-greeting");
 
 const USER_LS = "currentUser",
   SHOWING_CN = "showing";
-
+// LS에 key="currentUser", value="text로 input(입력)한 것"으로 각각 저장함
 function saveName(text) {
   localStorage.setItem(USER_LS, text);
 }
-
+// submit(제출) event가 발생하면 Default를 prevent(방지)하고
+// paintGreeting과 saveName이 실행되어 currentValue(=input.value)가 LS에 저장됨
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = input.value;
