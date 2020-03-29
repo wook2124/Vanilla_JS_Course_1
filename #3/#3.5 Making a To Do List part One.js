4ed44767796e5727dfc59f168ca20240d97f3b25
wook2124 <wook2124@naver.com>
2020-03-30 00:20:06 +0900

    Update #3.5 Making a To Do List part One.js

--- #3/#3.5 Making a To Do List part One.js
+++ #3/#3.5 Making a To Do List part One.js
@@ -1,7 +1,11 @@
-// index.html에 js-toDoForm,  js-toDoList class 만들기 
+// "js-toDoForm", "js-toDoList" class, <meta charset="utf-8" /> 추가 (delBtn인 ✖ 이모티콘이 읽히지 않음)
+// UTF-8은 유니코드를 위한 가변 길이 문자 인코딩 방식 중 하나로
+// Universal Coded Character Set + Transformation Format – 8-bit의 약자임
+// https://ko.wikipedia.org/wiki/UTF-8
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="utf-8" />
     <title>JH</title>
     <link href="style.css" rel="stylesheet" />
   </head>
@@ -24,7 +28,10 @@
   </body>
 </html>
 
-// User Name을 LS에 저장할 때와 같이 코드 작성! - init(initialize 초기화) 함수로 loadToDo와 submit(제출) event 발생하면 LS로 저장되게끔함!(여기서는 toDo가 null 값이 아닐경우에만 반응!)
+
+// User Name을 LS에 저장할 때와 같이 코드 작성 
+// init(initialize 초기화) 함수로 loadToDo와 handleSubmit를 실행시켜 LS에 저장되게끔함
+// 여기서는 toDo가 null 값이 아닐(!)경우에만 반응
 const toDoForm = document.querySelector(".js-toDoForm"),
   toDoInput = toDoform.querySelector("input"),
   toDoList = document.querySelector(".js-toDoList");
@@ -55,8 +62,9 @@ function init() {
 
 init();
 
+
 // toDoInput.value = "";
-// Enter를 치면 text가 console로 sunmit(제출)되고, Wirte a to do을 다시 빈칸이 되게함.
+// Enter를 치면 text가 console로 "sunmit"되고, "Wirte a to do"로 다시 빈칸이 채워짐
 function handleSubmit(event) {
   event.preventDefault();
   const currentValue = toDoInput.value;
@@ -64,8 +72,12 @@ function handleSubmit(event) {
   toDoInput.value = "";
 }
 
-// createElement - text를 입력하면 document에 li Element를 create(생성)하고 delBtn(delete Button), span Element 역시 document에 create(생성)해준다.
-// appendChild - span, delBtn을 따로따로 li에 append(추가)하고 마지막으로 li를 ul class인 toDoList에 append(추가)함!
+
+// createElement 
+// text를 입력하면 document에 li Element를 create(생성)하고 
+// delBtn(delete Button), span Element 역시 document에 create(생성)해줌
+// appendChild
+// span, delBtn을 따로따로 li에 append(추가)하고 마지막으로 li를 ul class인 toDoList에 append(추가)함
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
@@ -77,7 +89,42 @@ function paintToDo(text) {
   toDoList.appendChild(li);
 }
 
-// index.html 파일에 <meta charset="utf-8" /> 추가하기(delBtn인 ✖ 이모티콘이 읽히지 않음)
-// UTF-8은 유니코드를 위한 가변 길이 문자 인코딩 방식 중 하나로, 켄 톰프슨과 롭 파이크가 만들었다. UTF-8은 Universal Coded Character Set + Transformation Format – 8-bit 의 약자이다.
-// https://ko.wikipedia.org/wiki/UTF-8
-    <meta charset="utf-8" />
+
+// 최종 코드
+const toDoForm = document.querySelector(".js-toDoForm"),
+  toDoInput = toDoform.querySelector("input"),
+  toDoList = document.querySelector(".js-toDoList");
+
+const = TODO_LS = "toDo"
+
+function paintToDo(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText = "✖";
+  const span = document.createElement("span");
+  span.innerText = text;
+  li.appendChild(span);
+  li.appendChild(delBtn);
+  toDoList.appendChild(li);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = "";
+}
+
+function loadToDo() {
+    const toDo = localStorage.getItem(TODO_LS);
+    if(toDo !== null){
+
+    }
+}
+
+function init() {
+    loadToDo();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
