f928c97903a2be2774b362fd8327b86521f54e77
wook2124 <wook2124@naver.com>
2020-04-04 23:49:25 +0900

    Update #3.8 Image Background.js

--- #3/#3.8 Image Background.js
+++ #3/#3.8 Image Background.js
@@ -1,9 +1,10 @@
-// https://unsplash.com에서 무료로 배경사진을 다운!
-// index.html에 <script src="bg.js"></script> 추가!
-// Math.floor(), Math.ceil()
-// floor는 바닥, 즉 뒤에 있는 소숫점을 버림 / ceil은 천장, 즉 뒤에있는 소숫점을 올림(버림은 0에서 4까지, 올림은 1에서 5까지 숫자가 나옴)
-    <script src="bg.js"></script>
+// https://unsplash.com
+// index.html에 <script src="bg.js"></script> 추가
 
+
+// Math.floor(), Math.ceil()
+// floor는 바닥, 즉 뒤에 있는 소숫점을 버림
+// ceil은 천장, 즉 뒤에있는 소숫점을 올림
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 10;
@@ -20,15 +21,8 @@ function init() {
 init();
 
 
-// 다음으로 다운받은 image폴더 src(source)를 제공하고 body의 자손으로 추가하기(appendChilde), 그리고 addEventListen 하기!
-const body = document.querySelector("body");
-
-const IMG_NUMBER = 10;
-
-function handleImgLoad() {
-  console.log("finished loading");
-}
-
+// 다운받은 image들의 폴더 src(source) 제공
+// body의 자손 class로 image 추가(appendChild)
 function paintImage(imgNumber) {
   const image = new Image();
   image.src = `//images/${imgNumber}.jpg`;
@@ -36,21 +30,9 @@ function paintImage(imgNumber) {
   image.addEventListener("loadend", handleImgLoad);
 }
 
-function genNumber() {
-  const number = Math.ceil(Math.random() * IMG_NUMBER);
-  return number;
-}
 
-function init() {
-  const randomNumber = genNumber();
-  paintImage(randomNumber);
-}
-
-init();
-
-
-// image.classList.add('bgImage') - bgImage로 class 추가하기
-// stlye.css 파일도 수정
+// image.classList.add("bgImage") - bgImage로 class 추가
+// stlye.css 파일에서 bgImage class 수정
 function paintImage(imgNumber) {
   const image = new Image();
   image.src = `//images/${imgNumber}.jpg`;
@@ -68,8 +50,7 @@ function paintImage(imgNumber) {
 }
 
 
-// 배경화면이 뜨지 않아도 일단 과정대로 따라감
-// appendChild에서 prepend로 바꿔서 body 맨앞으로 가져옴
+// appendChild에서 prepend로 바꿔서 body 맨 앞으로 가져옴
 function paintImage(imgNumber) {
   const image = new Image();
   image.src = `//images/${imgNumber}.jpg`;
@@ -98,9 +79,7 @@ function paintImage(imgNumber) {
 }
 
 
-// 최종 코드
-// 배경화면이 뜨지 않은 이유는, 저장해둔 사진이 든 파일(Images)을 작업하고 있는 파일(bg.js)과 같은 위치에 뒀어야함! 
-// 그리고 addEventListen는 필요없어서 funcion과 함께 Delete!
+// 최종 코드 
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 10;
